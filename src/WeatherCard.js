beae52bce2f8cc633d4fd313cbe03e122bd237a8
Hasibullah khan <Hasibpak@gmail.com>
2020-10-14 21:11:57 +0400

    update app.css ,index.css Weathercard.js
    update app.css ,index.css Weathercard.js

--- src/WeatherCard.js
+++ src/WeatherCard.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import './App.css';
 const WeatherCard = (props) => {
     return (
         <div className="card">
-            <h1>Karachi</h1>
-            <h3>Pak</h3>
-            <img src="" alt="Weather Icon" />
-            <h1>34 °C</h1>
-            <h3>Clouds</h3>
+            <h1 className="city">Karachi</h1>
+            <h3 className="country">Pak</h3>
+            <img className="icon" src="" alt="Weather Icon" />
+            <h1 className="temp">34 °C</h1>
+            <h3 className="condition">Clouds</h3>
+
         </div>
+
     );
 }
 
