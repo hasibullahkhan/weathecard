755d5abf89757efaaad161e7d1e7aa03e8337ddd
Hasibullah khan <Hasibpak@gmail.com>
2020-10-15 19:34:32 +0400

    update app.css,weatherCard.js,add img folder in public folder copy file cloudy.png
    update app.css,weatherCard.js,add img folder in public folder copy file cloudy.png

--- src/WeatherCard.js
+++ src/WeatherCard.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import './App.css';
+
 const WeatherCard = (props) => {
     return (
         <div className="card">
-            <h1 className="city">Karachi</h1>
-            <h3 className="country">Pak</h3>
-            <img className="icon" src="" alt="Weather Icon" />
+
+            <div className="location">
+                <h1 className="city">Karachi</h1>
+                <h3 className="country">Pak</h3>
+            </div>
+
+            <img className="icon" src="./img/Cloudy.png" alt="Weather Icon" />
+
+            {/*  D:\react projects\weathercard\weathecard\public\img */}
             <h1 className="temp">34 °C</h1>
             <h3 className="condition">Clouds</h3>
-
+            <name>
+                <h6 className="name">-------------------------------------------------</h6>
+                <h6 className="name">Design by:Hasibullah khan</h6>
+                <h6 className="name">Email    :hasibpak@gmail.com</h6>
+            </name>
         </div>
 
     );
