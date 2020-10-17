13a84ab49e4b501a2d5958e420980fdd139ca8d4
Hasibullah khan <Hasibpak@gmail.com>
2020-10-18 00:18:21 +0400

    Update App.js,WeatherCard.js
    Update App.js,WeatherCard.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div>
-      < WeatherCard temp="36" imagecondition="Hail" city="Karachi" country="Pak" creatdby="Hasibullah khan" email="hasibpak@gmail.com" />
+      < WeatherCard temp="13" imagecondition="Hail" city="Karachi" country="Pak" creatdby="Hasibullah khan" email="hasibpak@gmail.com" />
     </div>
   );
 }
--- src/Components/WeatherCard.js
+++ src/Components/WeatherCard.js
@@ -6,10 +6,35 @@ import WeatherTemprature from './WeatherTemp'
 import Designby from './Designby';
 const WeatherCard = ({ temp, imagecondition, city, country, creatdby, email }) => {
 
+    // let temp = 15;
+    let heighColor = 0;
+    let lowColor = 0;
+    let bg = null;
+
+    if (temp > 12) { //this is for hot weather
+        heighColor = (1 - ((temp - 12) / 28)) * 255;
+        lowColor = heighColor - 200;
+        bg = `linear-gradient(to top,
+        rgb(255,${heighColor},0),
+        rgb(255,${Math.abs(lowColor)},0)
+        )`
+    } else if (temp <= 12) { // this is for Clod weather
+
+        heighColor = (1 - (temp + 20) / 32) * 255;
+        lowColor = heighColor - 200;
+        bg = `linear-gradient(to top,
+        rgb(0,${heighColor},255),
+        rgb(0,${Math.abs(lowColor)},255)
+        )`;
+    }
+
+
     const red = 100;
+
     const Card = styled.div`
                     margin:0 auto;
-                background: linear-gradient(to bottom,rgba(${red},200,200),lightblue);
+                /* background: linear-gradient(to bottom,rgba(${red},200,200),lightblue); */
+     background: ${bg};           
                 width: 200px;
                 height: 280px;
                 display: flex;
