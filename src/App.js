b4e2675ead3791397a2c975f3cc1525577ad6219
Hasibullah khan <Hasibpak@gmail.com>
2020-10-19 17:54:08 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import './App.css';
 function App() {
   const [city, setCity] = useState("");
   const [country, setCountry] = useState("");
-  const [weatherCond, setWeatherCond] = useState("");
-  const [weatherTemp, setWeatherTemp] = useState("");
+  const [weathercond, setWeatherCond] = useState("");
+  const [weathertemp, setWeatherTemp] = useState("");
 
   const q = "Lahore"
   const data = async () => {
@@ -17,19 +17,21 @@ function App() {
     const resJOSN = await apiRes.json()
     return resJOSN;
   }
+
+  data().then(res => {
+    setWeatherTemp(res.main.temp);
+    setWeatherCond(res.weather[0].main);
+    setCity(res.name)
+    setCountry(res.sys.country)
+  })
   const handleSearch = e => {
     e.PreventDefault();
-    data().then(res => {
-      console.log("feels_like " + res.main.feels_like)
-      console.log("the temp is " + res.main.temp)
-    })
+
   }
 
   return (
     <div>
-      < WeatherCard temp="3" imagecondition="Rain" city="Karachi" country="Pak" creatdby="Hasibullah khan" email="hasibpak@gmail.com" />
-      < WeatherCard temp="20" imagecondition="Dust" city="Lahore" country="Pak" creatdby="Hasibullah khan" email="hasibpak@gmail.com" />
-      < WeatherCard temp="40" imagecondition="Tornado" city="Sibhi" country="Pak" creatdby="Hasibullah khan" email="hasibpak@gmail.com" />
+      < WeatherCard temp={weathertemp} imagecondition={weathercond} city={city} country={country} creatdby="Hasibullah khan" email="hasibpak@gmail.com" />
     </div>
   );
 }
