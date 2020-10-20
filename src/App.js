9a322fe7584623bbfea999bdd4706314bd596e2c
Hasibullah khan <Hasibpak@gmail.com>
2020-10-20 18:32:31 +0400

    update app.js
    update app.js

--- src/App.js
+++ src/App.js
@@ -3,29 +3,30 @@ import WeatherCard from './Components/WeatherCard'
 import './App.css';
 
 function App() {
-  const [city, setCity] = useState("Lahore");
+  const [city, setCity] = useState("Sydeny, au");
   const [country, setCountry] = useState("");
   const [weathercond, setWeatherCond] = useState("");
   const [weathertemp, setWeatherTemp] = useState("");
 
-  const q = "";
+  const q = { city };
   const data = async () => {
     const apiRes = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=34b5f955b8fa96d8a786a271305fea87`
+      // `http://api.openweathermap.org/data/2.5/weather?q=${q}&units=metric&APPID=34b5f955b8fa96d8a786a271305fea87`
+      `http://api.openweathermap.org/data/2.5/weather?q=${q}&units=metric&APPID=34b5f955b8fa96d8a786a271305fea87`
 
     )
     const resJOSN = await apiRes.json()
     return resJOSN;
   }
 
-  // data().then(res => {
-  //   setWeatherTemp(res.main.temp);
-  //   setWeatherCond(res.weather[0].main);
-  //    setCity(res.name)
-  //   setCountry(res.sys.country)
-  // })
-  const handleSearch = e => {
-    e.PreventDefault();
+  data().then(res => {
+    setWeatherTemp(res.main.temp);
+    setWeatherCond(res.weather[0].main);
+    setCity(res.name)
+    setCountry(res.sys.country)
+  })
+  const handleSearch = (e) => {
+    e.preventDefault();
     data().then(res => {
       setWeatherTemp(res.main.temp);
       setWeatherCond(res.weather[0].main);
@@ -44,7 +45,9 @@ function App() {
         email="hasibpak@gmail.com" />
 
       <form>
-        <input value={city} onChange={e => setCity(e.target.value)} ></input>
+        <input
+          value={city}
+          onChange={(e) => setCity(e.target.value)} />
         <button onClick={e => handleSearch(e)}>Search</button>
       </form>
 
